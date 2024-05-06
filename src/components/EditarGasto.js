beb61b1b0551da732a88a23b7c47f3e7da0740d6
joelortiiz <joelortiizgarcia@gmail.com>
2024-05-06 09:52:28 +0200

    Componentes creados

---
+++ src/components/EditarGasto.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EditarGasto = () => {
+  return (
+    <div>EditarGasto</div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import WebFont from 'webfontloader';
+import Contenedor from './elementos/contenedor';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+  WebFont.load({
+    google: {
+      families: ['Work Sans: 400,500,700', 'sans-serif']
+    }
+  });
+
+const Index = () => {
+  return (
+    <BrowserRouter>
+    <Contenedor><App />
+    </Contenedor>
+    </BrowserRouter>
+  
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Index />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
